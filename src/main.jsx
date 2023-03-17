f0cb3a003581cb0cfa52cbb2afd1902c700148ad
WagnerRodrigues <felten19@gmail.com>
2023-03-17 19:18:45 -0300

    Criada as configuraçoes Style-global da aplicacao

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./Pages/App";
 
 import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./style/global";
 import theme from "./style/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
