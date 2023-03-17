bd8fc5838e8d00aca7e4fa86cc1447440bca378d
WagnerRodrigues <felten19@gmail.com>
2023-03-17 19:29:14 -0300

    colocando os links das fontes do projeto no index.html

--- src/style/global.js
+++ src/style/global.js
@@ -6,7 +6,18 @@ export default createGlobalStyle `
         margin: 0;
         box-sizing: border-box;
     }
+    
     body{
         background-color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+        color: ${({theme}) => theme.COLORS.WHITE};
+    }
+
+    a{
+        text-decoration: none;
+    }
+
+    button, a{
+        cursor: pointer;
+        transition: filter 0.2s;
     }
 `;
