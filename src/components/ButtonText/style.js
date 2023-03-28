b6002b1db278f2880e1f3394c718412b5bfad717
WagnerRodrigues <felten19@gmail.com>
2023-03-28 10:06:08 -0300

    Estelizado e feito uma logica para quando o bottun tiver ativo

--- src/components/ButtonText/style.js
+++ src/components/ButtonText/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ContainerText = styled.button`
     background-color: ${({theme}) => theme.COLORS.BACKGROUND_800};
-    color: ${({theme}) => theme.COLORS.ORANGE};
+    color: ${({theme, isActive}) => isActive ? theme.COLORS.GRAY_100 :theme.COLORS.ORANGE};
    
     font-size: 18px;
     border: none;
