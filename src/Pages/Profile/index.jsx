bf9e520afc126441afce0012e93f8d6d03809859
WagnerRodrigues <felten19@gmail.com>
2023-04-13 14:03:13 -0300

    Criado page de atualizacao de cadastro do usuario

---
+++ src/Pages/Profile/index.jsx
@@ -0,0 +1,33 @@
+import { Container, Form, Avatar } from "./style.js";
+import {FiArrowLeft, FiCamera, FiLock, FiMail, FiUser} from "react-icons/fi";
+import Input from "../../components/Input";
+import Buttons from "../../components/Buttons";
+
+export default function Profile(){
+    return(
+        <Container>
+            <header>
+                 <FiArrowLeft />
+            </header>
+
+            <Form>
+                <Avatar>
+                    <img src="https://github.com/wagnerfelten.png" alt="Foto do Usuário"/>
+                    
+                    <label htmlFor="avatar">
+                        <FiCamera />
+
+                        <input id="avatar" type="file"/>
+                    </label>
+                </Avatar>
+
+                <Input placeholder="Nome" type="text" icon={FiUser}/>
+                <Input placeholder="E-mail" type="text" icon={FiMail}/>
+                <Input placeholder="Senha atual" type="password" icon={FiLock}/>
+                <Input placeholder="Nova senha" type="password" icon={FiLock}/>
+
+                <Buttons title="Atualizar"/>
+            </Form>
+        </Container>
+    );
+}
---
+++ src/Pages/Profile/style.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+    width: 100%;
+
+    > header{
+        width: 100%;
+        height: 144px;
+        background-color: ${({theme}) => theme.COLORS.BACKGROUND_900};
+
+        display: flex;
+        align-items: center;
+
+        padding: 0 124px;
+    
+        svg {
+            font-size: 34px;
+        }
+    }
+
+`;
+
+export const Form = styled.form `
+    width: 400px;
+    
+    display: flex;
+    flex-direction: column;
+    
+
+    margin: 0 auto;
+`;
+
+export const Avatar = styled.div `
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   margin: 0px auto 30px;
+   margin-top: -80px;
+   position: relative;
+
+   > div:nth-child(3){
+        margin-top: 24px;
+   }
+
+   > img{
+    width: 150px;
+    border-radius: 50%;
+   }
+
+   > label {
+    width: 48px;
+    height: 48px;
+
+    background-color: ${({theme}) => theme.COLORS.ORANGE};
+
+    border-radius: 50%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: absolute;
+    bottom: 2px;
+    right: 2px;
+
+    
+    
+    cursor: pointer;
+
+    input{
+        display: none;
+    }
+
+    svg{
+        width: 24px;
+        height: 24px;
+        color: ${({theme})=> theme.COLORS.BACKGROUND_900};
+    }
+   }
+`;
