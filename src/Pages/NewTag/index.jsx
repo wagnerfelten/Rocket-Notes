148c719a16c86df1bb9e04876fb98e713aec7fe4
WagnerRodrigues <felten19@gmail.com>
2023-04-13 23:43:16 -0300

    Criado page para criação de tags

---
+++ src/Pages/NewTag/index.jsx
@@ -0,0 +1,44 @@
+import { Container, Form } from "./styled";
+
+import Textarea from "../../components/Textarea";
+import NoteItem from "../../components/NoteItem";
+import Section from "../../components/Section";
+import Button from "../../components/Buttons";
+import Header from "../../components/Header";
+
+
+export default function NewTag() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar Nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+
+          <Input placeholder="Titúlo" />
+
+          <Textarea placeholder="Observações" />
+          <Input placeholder="Observacoes" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://google.com" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Enviar" />
+        </Form>
+      </main>
+    </Container>
+  );
+}
