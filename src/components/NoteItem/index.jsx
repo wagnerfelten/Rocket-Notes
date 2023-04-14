17f27d6c0824a1307f155cbb2ceee8cf7c892cee
WagnerRodrigues <felten19@gmail.com>
2023-04-13 23:46:50 -0300

    componente de criacao de itens na page Criar nota

---
+++ src/components/NoteItem/index.jsx
@@ -0,0 +1,15 @@
+import { FiPlus, FiX} from "react-icons/fi";
+
+import {Container} from "./style";
+
+export default function NoteItem({isNew, value, onClick, ...rest}){
+    return(
+        <Container isNew={isNew}>
+            <input type="text" value={value} readOnly={!isNew} />
+
+            <button onClick={onClick} className={isNew ? "button-add" : "button-delete"}>
+                {isNew ? <FiPlus /> : <FiX />}
+            </button>
+        </Container>
+    );
+}
